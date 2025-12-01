49bc6a70f51565a118d7cf8a1d24e3f84147d458
Ghost <duchojakub987@gmail.com>
2025-12-01 14:49:28 +0100

    day_one_finished

---
+++ day_01/src/lib.rs
@@ -0,0 +1,16 @@
+
+pub fn parse_instruction(instruction: &str) -> i32 {
+    let mut number = String::new();
+    let mut first_loop = false;
+    for char in instruction.chars() {
+        if !first_loop {
+            if char == 'L' {
+                number.push_str("-");
+            }           
+            first_loop = true;
+            continue;
+        }
+        number.push_str(char.to_string().as_str());
+    }
+    number.parse::<i32>().unwrap()
+}
--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -1,3 +1,60 @@
+use std::{fs::read_to_string, process, error::Error};
+use day_01::parse_instruction;
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = "input.txt";
+    if let Err(e) = run(file_name) {
+        eprintln!("Error: {e}");
+        process::exit(1);
+    }
+
 }
+
+fn run(file_name: &str) -> Result<(), Box<dyn Error>>{
+    let mut instructions: Vec<&str> = Vec::new();
+    
+    let binding = read_to_string(file_name)?;
+    for line in binding.lines() {
+        instructions.push(line);
+    }
+
+    let mut zero_count = 0;
+    let mut input_count = 50;
+    
+    for instruction in instructions {
+        println!("{input_count}");
+        let mut temp = parse_instruction(&instruction);
+        loop {
+            if temp > 0 {
+                input_count += 1;
+                temp -= 1;
+                if input_count > 99 {
+                    input_count = 0;
+                }
+                if input_count == 0 {
+                    zero_count += 1;
+                }
+                if temp <= 0 {
+                    break;
+                }
+            } 
+            else{
+                input_count -= 1; 
+                temp += 1;
+                if input_count < 0 {
+                    input_count = 99;
+                }
+                if input_count == 0 {
+                    zero_count += 1;
+                }
+                if temp >= 0 {
+                    break;
+                }
+            }
+        }
+        
+    }
+    println!("Result: {zero_count}");
+    Ok(())
+}
+
